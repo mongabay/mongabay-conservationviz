d1bfa614544985dfe7f6877c0f56023d56664947
Tom <tom.allnutt@gmail.com>
2017-06-24 11:40:19 -0700

    adds map, basic grouped icons

--- index.js
+++ index.js
@@ -1,29 +1,118 @@
-// initial D3 setup
-var width = 1000,
-    barHeight = 20;
+// globals
+var MAP, LABELS, DATA;
+DATA_COUNTRIES = {};
+CENTROIDS = {};
+var MIN_ZOOM = 2;
+var MAX_ZOOM = 18;
 
-var x = d3.scale.linear()
-    .domain([0, 10])
-    .range([0, width]);
+$(document).ready(function(){
+  initDOM();
+  initD3();
 
-// Select and append DOM elements to hold the chart viz
-var summary_chart = d3.select("body").append("table").attr("class","summary");
-var primary_chart = d3.select("body").append("table").attr("class","main");
-
-// Get the data and, when ready, create the charts
-d3.csv("data.csv", function(data) {
-  
-  // First, make the summary chart, by theme
-  makeChart(data, 'theme', summary_chart)
-
-  // Next, make the "primary chart", by variable
-  makeChart(data, 'variable', primary_chart, true)
-
-}); // d3.csv
+});
   
 ////////////////////////////////////////////////////
-// Utility functions
+// Named functions
 ////////////////////////////////////////////////////
+
+function initDOM() {
+  // Select and append DOM elements to hold the charts and map
+  var summary_chart = d3.select("body").append("table").attr("class","summary");
+  var map = d3.select('body').append("div").attr("id","map");
+  var primary_chart = d3.select("body").append("table").attr("class","primary");
+
+}
+
+function initD3() {
+  // initial D3 setup
+  var width = 1000,
+      barHeight = 20;
+
+  var x = d3.scale.linear()
+      .domain([0, 10])
+      .range([0, width]);
+
+  // structure county centoids for use below
+  d3.csv("data/countries.csv", function(countries){
+      countries.forEach(function(country){
+        var c = {
+          lat: country.latitude,
+          lng: country.longitude,
+          abbreviation: country.country,
+          count: 0,
+        }
+        DATA_COUNTRIES[country.name] = c;
+      });
+  });
+
+  // Get the data and, when ready, create the charts
+  d3.csv("data/data.csv", function(data) {
+    data.forEach(function(d){
+      var countries = d.country.split(',');
+      // at present, sum all - so stuides with multiple countries will get multiple icons
+      countries.forEach(function(country) {
+        // skip bad matches
+        if (DATA_COUNTRIES[country] === undefined) return;
+        DATA_COUNTRIES[country]["count"] = DATA_COUNTRIES[country]["count"] += 1;
+      })
+    })
+
+    // First, make the summary chart, by theme
+    makeChart(data, 'theme', d3.select("table.summary"));
+    // Next, make the "primary chart", by variable
+    makeChart(data, 'variable', d3.select("table.primary"), true);
+
+    // now we have the data we need for the map
+    initMap();
+
+  }); // d3.csv
+}
+
+
+function initMap() {
+  // init the map with some basic settings
+  MAP = L.map('map',{
+    minZoom: MIN_ZOOM,
+    maxZoom: MAX_ZOOM,
+    keyboard: false,
+  });
+
+  // add a positron basemap, without labels
+  var positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
+    attribution: '©OpenStreetMap, ©CartoDB'
+  }).addTo(MAP);
+
+  // then create a tile pane for the labels, and add positron labels to it (only at high zoom)
+  MAP.createPane('labels');
+  MAP.getPane('labels').style.zIndex = 650;
+  MAP.getPane('labels').style.pointerEvents = 'none';
+  LABELS = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_only_labels/{z}/{x}/{y}.png', {
+    attribution: '©OpenStreetMap, ©CartoDB',
+    pane: 'labels'
+  });
+
+  // add div icons to the map for each distinct country where count > 1 
+  var countries = Object.keys(DATA_COUNTRIES);
+  var markers = L.featureGroup().addTo(MAP);
+  countries.forEach(function(name){
+    if (DATA_COUNTRIES[name] === undefined) return;
+    if (DATA_COUNTRIES[name].count === undefined) return;
+    if (DATA_COUNTRIES[name].lat === undefined || DATA_COUNTRIES[name].lng === undefined) return;
+    if (DATA_COUNTRIES[name].count > 0) {
+      var country = DATA_COUNTRIES[name];
+      var icon = L.divIcon({
+        className: 'country-icon',
+        html: '<span class="icon-text">'+ country.count +'</span>'
+      });
+      var marker = L.marker([country.lat, country.lng], {icon: icon}).addTo(markers);
+    }
+
+  });
+
+  MAP.fitBounds(markers.getBounds());
+
+}
+
 
 /**
  * makes a chart of squares
@@ -128,15 +217,24 @@ var tooltip = d3.select("body")
     .append("div")
     .attr("class","tooltip");
 
+
+///////////////////////////////////////////////////////////////////////////////////
+// Shims and utilities
+///////////////////////////////////////////////////////////////////////////////////
+
+// L.TopoJSON to pretend that TopoJSON is just plain ol' GeoJSON
+L.TopoJSON=L.GeoJSON.extend({addData:function(a){var b=this;return"Topology"===a.type?Object.keys(a.objects).forEach(function(c){var d=topojson.feature(a,a.objects[c]);L.GeoJSON.prototype.addData.call(b,d)}):L.GeoJSON.prototype.addData.call(this,a),this}});
+
 // utility function to turn an integer into an array from 0 to n
 function range(n) {
-  var foo = [];
+  var array = [];
   for (var i = 0; i <= n - 1; i++) {
-    foo.push(i)
+    array.push(i)
   }
-  return foo;
+  return array;
 }
 
+// get distinct list by key from an array of objects 
 function get_distinct(array, key) {
   var unique = {};
   var distinct = [];
@@ -147,12 +245,4 @@ function get_distinct(array, key) {
     unique[array[i][key]] = 0;
   }
   return distinct;
-}
-
-function compare(a,b) {
-  if (a.last_nom < b.last_nom)
-    return -1;
-  if (a.last_nom > b.last_nom)
-    return 1;
-  return 0;
 }
