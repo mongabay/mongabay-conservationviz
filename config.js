770e5457bfdcbe832c228c876be73eaa1b4a91a0
Tom <tom.allnutt@gmail.com>
2017-07-19 12:00:55 -0700

    add note to margins

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ config[themes.top] = {
   svgwidth:   680,                // width of the svg container, TO DO
   svgwidthscaled: 680 - 140,      // TO DO, why is <g> bigger than <svg>?
   svgmargin:  { top:    0,        // margins around the svg container
-                bottom: 0, 
+                bottom: 0,        // TO DO, needs more investigation
                 right:  0, 
                 left:   100 }, 
   ncols:      1,                  // number of chart cols
@@ -23,7 +23,7 @@ config[themes.bottom] = {
   svgwidth:   800,                // width of the svg container, TO DO
   svgwidthscaled: 809 - 140,      // TO DO, why is <g> bigger than <svg>?
   svgmargin:  { top:    0,        // margins around the svg container
-                bottom: 0, 
+                bottom: 0,        // TO DO, needs more investigation
                 right:  0, 
                 left:   0 }, 
   ncols:      3,                  // number of chart cols
