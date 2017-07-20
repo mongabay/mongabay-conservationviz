33a29424328e26d69a791863c05fb4688bdfee06
Tom <tom.allnutt@gmail.com>
2017-07-20 16:16:44 -0700

    much refactoring towards columns, positioning, text wrapping

--- config.js
+++ config.js
@@ -10,24 +10,27 @@ var themes = {
 // Note: additional config variables are calculated at runtime and added to this structure
 var config = {};
 config[themes.top] = {
-  svgwidth:   680,                // width of the svg container, TO DO
-  svgwidthscaled: 680 - 140,      // TO DO, why is <g> bigger than <svg>?
   svgmargin:  { top:    0,        // margins around the svg container
-                bottom: 0,        // TO DO, needs more investigation
+                bottom: 0,        
                 right:  0, 
-                left:   100 }, 
+                left:   100 },    // left margin will hold the text labels
+  svgwidth:   580,                // width of the svg container, TO DO
   ncols:      1,                  // number of chart cols
   sqsize:     15,                 // width and hight of chart squares
   rowpadding: 10,                 // padding between rows
 };
 config[themes.bottom] = {
-  svgwidth:   800,                // width of the svg container, TO DO
-  svgwidthscaled: 809 - 140,      // TO DO, why is <g> bigger than <svg>?
-  svgmargin:  { top:    0,        // margins around the svg container
-                bottom: 0,        // TO DO, needs more investigation
+  svgwidth:   700,       
+  svgmargin:  { top:    0,        
+                bottom: 0,        
                 right:  0, 
-                left:   0 }, 
-  ncols:      3,                  // number of chart cols
-  sqsize:     20,                 // width and hight of chart squares
-  rowpadding: 10,                 // padding between rows
+                left:   120 },      
+  ncols:      3,                 
+  sqsize:     20,                 
+  rowpadding: 10,                 
 };
+
+// update things that depend on the above config being set
+// ahh the mysteries of svg
+config[themes.top]["svgwidthscaled"] = config[themes.top]["svgwidth"] - (config[themes.top]["svgmargin"]["left"] * 1.2);
+config[themes.bottom]["svgwidthscaled"] = config[themes.bottom]["svgwidth"] - config[themes.bottom]["svgmargin"]["left"] - 100;
