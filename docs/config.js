2e5f0d2381d5e6eb648701380c8bf19606d66892
Tom <tom.allnutt@gmail.com>
2017-10-05 17:27:17 -0700

    update docs

--- docs/config.js
+++ docs/config.js
@@ -19,10 +19,22 @@ var words = { fsc: [], pes: [] };
 words["fsc"][colgroups[0]] = "Mostly positive";
 words["fsc"][colgroups[1]] = "More evidence needed";
 words["fsc"][colgroups[2]] = "Inconclusive (mixed results)";
+words["pes"][colgroups[0]] = "Mostly positive, some red flags";
+words["pes"][colgroups[1]] = "Mostly little change, some red flags";
+words["pes"][colgroups[2]] = "More evidence needed";
+
+// fullscreen titles, subtitles
+var fullscreentitle = { fsc: '', pes: '' };
+fullscreentitle['fsc'] = 'The scientific evidence on tropical forest certification';
+fullscreentitle['pes'] = 'The scientific evidence on Payments for Ecosystem Services';
+var fullscreensubtitle = { fsc: '', pes: '' };
+fullscreensubtitle['fsc'] = 'Is certified forest management really better than conventional logging for the environment, people, and logging companies’ bottom lines?';
+fullscreensubtitle['pes'] = 'Is paying landowners in the tropics for providing ecosystem services better for the environment and the landowners than doing nothing?';
 
 // the description, below the map, to the left of the legend
 var description = { fsc: '', pes: '' };
 description["fsc"] = '<h3>How to read this infographic</h3><p>The map shows countries where scientists have measured the effectiveness of FSC certification or Reduced Impact Logging. Try hovering or clicking on a circle &mdash; the more evidence there is, the larger the circle.</p><p>The squares below show the results of the studies we have reviewed (see <a href="https://news.mongabay.com/conservation-effectiveness/how-we-reviewed-the-evidence/" target="_blank">methods</a>). Each square (try clicking on one) represents one data point extracted from <a href="https://news.mongabay.com/conservation-effectiveness/references-for-story-on-forest-certification/" target="_blank">scientific, peer-reviewed literature</a>.</p>';
+description["pes"] = '<h3>How to read this infographic</h3><p>The map shows countries where scientists have measured the effectiveness of Payments for Ecosystem Services (PES). Try hovering or clicking on a circle — the more evidence there is, the larger the circle.</p><p>The squares below show the results of the studies we have reviewed (see <a href="https://news.mongabay.com/conservation-effectiveness/how-we-reviewed-the-evidence/" target="_blank">methods</a>). Each square (try clicking on one) represents one data point extracted from <a href="https://news.mongabay.com/conservation-effectiveness/references-for-story-on-PES/" target="_blank">scientific, peer-reviewed literature</a>.</p>';
 
 // horizontal legend text
 var legend_text = { fsc: [], pes: [] };
@@ -30,7 +42,12 @@ legend_text["fsc"] = [
   "Certified better than conventional",
   "Certified same as conventional",
   "Certified worse than conventional"
-]
+];
+legend_text["pes"] = [
+  "PES better than no PES",
+  "PES same as no PES",
+  "PES worse than no PES"
+];
 
 // map circle areas, in km2
 var circleareas = {
