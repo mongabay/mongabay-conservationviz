52655192e4365c87961cca840d789489a0ff750b
Tom <tom.allnutt@gmail.com>
2017-12-07 10:34:41 -0800

    testing config changes for variable col names/types

--- config.js
+++ config.js
@@ -1,32 +1,23 @@
 ///////////////////////////////////////////////
-// Theme groups
-////////////////////////////////////////////////
-
-// theme groups (identified as a column in each data.csv row)
-// by which to group rows on top and bottom charts
-var groups = {
-  top: "theme",
-  bottom: "variable"
-}
-
-///////////////////////////////////////////////
 // Strategy-specific variables
 ////////////////////////////////////////////////
 
 // define the universe of strategies. These same acronyms are also expected in the url switch that tells the app which strategy to show, e.g. localhost:8000/?cfm
-var strategies = ['fsc','pes','cfm'];
+var strategies = ["fsc","pes","cfm","pas"];
 
 // url to "full screen" version of app
 var fullscreen = {};
 fullscreen["fsc"] = "https://mongabay-imgs.s3.amazonaws.com/vz/fullscreen/index.html?fsc"
 fullscreen["pes"] = "https://mongabay-imgs.s3.amazonaws.com/vz/fullscreen/index.html?pes"
 fullscreen["cfm"] = "https://mongabay-imgs.s3.amazonaws.com/vz/fullscreen/index.html?cfm"
+fullscreen["pas"] = "https://mongabay-imgs.s3.amazonaws.com/vz/fullscreen/index.html?pas"
 
 // the link back to the article on mb.com
 var articlelink = {};
 articlelink["fsc"] = "https://news.mongabay.com/2017/09/does-forest-certification-really-work/";
 articlelink["pes"] = "https://news.mongabay.com/2017/10/cash-for-conservation-do-payments-for-ecosystem-services-work/";
 articlelink["cfm"] = "https://news.mongabay.com/2017/11/does-community-based-forest-management-work-in-the-tropics";
+articlelink["pas"] = "https://news.mongabay.com/2017/11/does-community-based-forest-management-work-in-the-tropics";
 
 // keys to the column groups. These will be used below, and throughout to 
 // identify and organize groups from the data
@@ -34,6 +25,7 @@ var colgroups = {};
 colgroups["fsc"] = ["env", "soc", "econ"];
 colgroups["pes"] = ["env", "soc", "econ"];
 colgroups["cfm"] = ["env", "soc", "econ"];
+colgroups["pas"] = ["env", "soc", "econ"];
 
 // descriptive summaries for the theme groups
 var words = {};
@@ -49,22 +41,30 @@ words["cfm"] = [];
 words["cfm"][colgroups["cfm"][0]] = "Mostly no or positive changes";
 words["cfm"][colgroups["cfm"][1]] = "Inconclusive (mixed results)";
 words["cfm"][colgroups["cfm"][2]] = "Inconclusive (insufficient evidence)";
+words["pas"] = [];
+words["pas"][colgroups["pas"][0]] = "First conclusion";
+words["pas"][colgroups["pas"][1]] = "Second conclusion";
+words["pas"][colgroups["pas"][2]] = "Third conclusion";
 
 // fullscreen titles, subtitles
 var fullscreentitle = {};
-fullscreentitle['fsc'] = 'The scientific evidence on tropical forest certification';
-fullscreentitle['pes'] = 'The scientific evidence on Payments for Ecosystem Services';
-fullscreentitle['cfm'] = 'The scientific evidence on community-based forest management';
+fullscreentitle["fsc"] = "The scientific evidence on tropical forest certification";
+fullscreentitle["pes"] = "The scientific evidence on Payments for Ecosystem Services";
+fullscreentitle["cfm"] = "The scientific evidence on community-based forest management";
+fullscreentitle["pas"] = "The scientific evidence on community-based forest management";
 var fullscreensubtitle = {};
-fullscreensubtitle['fsc'] = 'Is certified forest management really better than conventional logging for the environment, people, and logging companies’ bottom lines?';
-fullscreensubtitle['pes'] = 'Is paying landowners in the tropics for providing ecosystem services better for the environment and the landowners than doing nothing?';
-fullscreensubtitle['cfm'] = 'Is community-based forest management good for forests in countries that lie in the tropics, and the people who live near those forests?';
+fullscreensubtitle["fsc"] = "Is certified forest management really better than conventional logging for the environment, people, and logging companies’ bottom lines?";
+fullscreensubtitle["pes"] = "Is paying landowners in the tropics for providing ecosystem services better for the environment and the landowners than doing nothing?";
+fullscreensubtitle["cfm"] = "Is community-based forest management good for forests in countries that lie in the tropics, and the people who live near those forests?";
+fullscreensubtitle["pas"] = "Is community-based forest management good for forests in countries that lie in the tropics, and the people who live near those forests?";
 
 // the description, below the map, to the left of the legend
+// Note: enter this as HTML. Also: use single quotes outside, and double-quotes inside (e.g. target="_blank")
 var description = {};
 description["fsc"] = '<h3>How to read this infographic</h3><p>The map shows countries where scientists have measured the effectiveness of FSC certification or Reduced Impact Logging. Try hovering or clicking on a circle &mdash; the more evidence there is, the larger the circle.</p><p>The squares below show the results of the studies we have reviewed (see <a href="https://news.mongabay.com/conservation-effectiveness/how-we-reviewed-the-evidence/" target="_blank">methods</a>). Each square (try clicking on one) represents one data point extracted from <a href="https://news.mongabay.com/conservation-effectiveness/references-for-story-on-forest-certification/" target="_blank">scientific, peer-reviewed literature</a>.</p>';
 description["pes"] = '<h3>How to read this infographic</h3><p>The map shows countries where scientists have measured the effectiveness of Payments for Ecosystem Services (PES). Try hovering or clicking on a circle — the more evidence there is, the larger the circle.</p><p>The squares below show the results of the studies we have reviewed (see <a href="https://news.mongabay.com/conservation-effectiveness/how-we-reviewed-the-evidence/" target="_blank">methods</a>). Each square (try clicking on one) represents one data point extracted from <a href="https://news.mongabay.com/conservation-effectiveness/references-for-story-on-PES/" target="_blank">scientific, peer-reviewed literature</a>.</p>';
 description["cfm"] = '<h3>How to read this infographic</h3><p>The map shows countries where scientists have measured the effectiveness of community forest management initiatives. Try hovering or clicking on a circle — the more evidence there is, the larger the circle.</p><p>The squares below show the results of the studies we have reviewed <a href="https://news.mongabay.com/conservation-effectiveness/how-we-reviewed-the-evidence/" target="_blank">methods</a>). Each square (try clicking on one) represents one data point extracted from <a href="https://news.mongabay.com/conservation-effectiveness/references-for-does-community-based-forest-management-work-in-the-tropics/" target="_blank">scientific, peer-reviewed literature</a>.</p>';
+description["pas"] = '<h3>How to read this infographic</h3><p>The map shows countries where scientists have measured the effectiveness of community forest management initiatives. Try hovering or clicking on a circle — the more evidence there is, the larger the circle.</p><p>The squares below show the results of the studies we have reviewed <a href="https://news.mongabay.com/conservation-effectiveness/how-we-reviewed-the-evidence/" target="_blank">methods</a>). Each square (try clicking on one) represents one data point extracted from <a href="https://news.mongabay.com/conservation-effectiveness/references-for-does-community-based-forest-management-work-in-the-tropics/" target="_blank">scientific, peer-reviewed literature</a>.</p>';
 
 // horizontal legend text
 var legend_text = {};
@@ -79,6 +79,11 @@ legend_text["pes"] = [
   "PES worse than no PES"
 ];
 legend_text["cfm"] = [
+  "CFM better than no CFM",
+  "CFM same as no CFM",
+  "CFM worse than no CFM"
+];
+legend_text["pas"] = [
   "CFM better than no CFM",
   "CFM same as no CFM",
   "CFM worse than no CFM"
@@ -107,4 +112,14 @@ var config = {
   buttonheight: 34,    // height of the button headers at top of row
 };
 
+///////////////////////////////////////////////
+// Theme groups
+////////////////////////////////////////////////
 
+// theme groups (identified as a column in each data.csv row)
+// by which to group rows on top and bottom charts
+// these were more relevant in the previous versions of the app which had charts both above and below the map
+var groups = {
+  top: "theme",
+  bottom: "variable"
+}
